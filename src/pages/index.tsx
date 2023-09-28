0770766bb90a173b71f5ec99827df242745bd077
AndreChavs <79666974+AndreChavs@users.noreply.github.com>
2023-09-28 16:50:38 -0300

    correção final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,60 +2,41 @@ import React from "react";
 import { fetchUserIds } from "@/functions/fetchUsersIds"
 import { checkStatus } from "@/functions/checkStatus"
 import { sendEmail } from "@/functions/sendEmail"
+import { GetStaticProps } from "next";
 
-
-
-export default function Home() {
-  const [userData, setUserData] = React.useState<string[]>([])
-
-
-  React.useEffect(() => {
-    let data: string[] = []
-    async function main(
-      usuarios:() => Promise<string[]>,
-      statusCheck:(value:string) => Promise<{status:string, id:string}>,
-      emailSend:(value:string) => Promise<boolean>,
-      setUserData:React.Dispatch<React.SetStateAction<string[]>>
-      ) {
-      try {
-        const users = await usuarios();
-        users.forEach(async (user) => {
-          const userCheck = await statusCheck(user);
-          if(userCheck.status === 'online'){
-            const userSend = await emailSend(userCheck.id);
-            if (userSend) {
-              data.push(userCheck.id)
-            }
-          }
-          setUserData(data)
-          // console.log(data)
-        })
-      } catch (error) {
-        console.error(`DEU RUIM: ${error}`)
-      }
+export const getStaticProps:GetStaticProps = async () => {
+  const users = await fetchUserIds()
+  const promise = users.map(async (user) => {
+    const userCheck = await checkStatus(user);
+    if(userCheck.status === 'online'){
+      const userSend = await sendEmail(userCheck.id)
+      if(userSend){
+        console.log(userCheck.id)
+        return userCheck.id
+      }      
     }
-    console.log(userData.length)
-    if(userData.length === 0){
-      main(fetchUserIds,checkStatus,sendEmail,setUserData)
-    }
-    
-    
-    
-  },[])
+    return null
+  });
+  const results = await Promise.all(promise)
+  const data = results.filter((user) => user !== null)
+  return {
+    props:{data}
+  }
+}
+
+export default function Home({data}:{data:string[]}) {
+  const [userData, setUserData] = React.useState<string[]>(data)  
+
   function render() {
-    if (userData) {
-      return userData.map((user) => {
-        console.log(user)
-        return <li key={user}>{user}</li>
-      })
-    }else {
-      return <p>Erro ao Buscar Dados</p>
-    }
+    return userData.map((user) => {
+      return <li key={user}>{user}</li>
+    })
   }
     
   return (
     <>
-     {render() && render()}
+     {render()}
     </>
   )
 }
+
